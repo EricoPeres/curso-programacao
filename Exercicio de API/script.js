f01f92d7cede8d4c8dbdd0e1574d670ea47d5bb4
EricoPeres <ericostefanni@gmail.com>
2025-05-07 12:59:03 -0300

    commit inicial consulmo API

---
+++ Exercicio de API/script.js
@@ -0,0 +1,7 @@
+// URL da API
+const URL = ''
+
+// Elementos HTML
+let $fatos_p = document.getElementById("facts")
+let $btn_pegar_fatos = document.getElementById("btn_get_facts")
+
--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -2,40 +2,57 @@
 let $tarefa_input = document.getElementById("txt_input")  // $ é a convensão para que tenha um elemento html
 let $erro_msg = document.getElementById("erro_msg")
 let $tarefas_lista = document.getElementById("tarefas_lista")
+let $btn_add_tarefa = document.getElementById("btn_addTarefa")
 
-function AdicionarTarefa() {
-    $erro_msg.textContent = ""
+CarregarTarefas()
+
+$btn_add_tarefa.addEventListener('click', () => {
     let tarefa_txt = $tarefa_input.value.trim()  // trim é para tirar os espaços do que digitado no input
-
     if(tarefa_txt !== "") {
-        // Criando os elementos
-        let $tarefa_item = document.createElement("li")
-        let $btn_completar = document.createElement("button")
-        let $btn_deletar = document.createElement("button")
-
-        // Adicionando conteúdo no elemento
-        $tarefa_item.innerText = tarefa_txt
-        $btn_completar.innerText = "Completa"        
-        $btn_deletar.innerText = "Deletar"
-
-        // Adicionando o elemento na página        
-        $tarefa_item.appendChild($btn_completar)
-        $tarefa_item.appendChild($btn_deletar)
-        $tarefas_lista.appendChild($tarefa_item)
-
-        // Tarefa concluída
-        $btn_completar.addEventListener('click', () => {
-            $tarefa_item.classList.add("completa")
-        })
-            
-        // Deletar a tarefa
-        $btn_deletar.addEventListener('click', () => $tarefa_item.remove())
+        AdicionarTarefa(tarefa_txt)
         $tarefa_input.value = ""
-        SalvarTarefa() 
-
     }else{
         $erro_msg.textContent = "Digite uma tarefa!"
     }
+})
+
+function AdicionarTarefa(texto, completada = false) {
+    
+    $erro_msg.textContent = ""
+
+    // Criando os elementos
+    let $tarefa_item = document.createElement("li")
+    let $btn_completar = document.createElement("button")
+    let $btn_deletar = document.createElement("button")
+
+    // Adicionando conteúdo no elemento    
+    $btn_completar.innerText = "Completa"        
+    $btn_deletar.innerText = "Deletar"
+    $tarefa_item.innerText = texto
+
+    // Adicionando o elemento na página        
+    $tarefa_item.appendChild($btn_completar)
+    $tarefa_item.appendChild($btn_deletar)
+    $tarefas_lista.appendChild($tarefa_item)
+
+    if(completada) {
+        $tarefa_item.classList.add("completa")
+    }
+
+    // Tarefa concluída
+    $btn_completar.addEventListener('click', () => {
+        $tarefa_item.classList.add("completa")
+        SalvarTarefa()
+    })
+        
+    // Deletar a tarefa
+    $btn_deletar.addEventListener('click', () => {
+        $tarefa_item.remove()
+        SalvarTarefa()
+    })
+
+    $tarefa_input.value = ""
+    SalvarTarefa() 
 
 }
 
@@ -49,4 +66,11 @@ function SalvarTarefa() {
     })
 
     localStorage.setItem('tarefa', JSON.stringify(tarefas))
+}
+
+function CarregarTarefas() {
+    let tarefas = JSON.parse(localStorage.getItem('tarefa'))   // transformar uma string em um objeto
+    tarefas.forEach( tarefa => {
+        AdicionarTarefa(tarefa.texto, tarefa.completada)
+    })
 }
